adc21fda80e11d5acdc5176c60c511971b9c99e3
YuriDomingos <josemarlabareda@gmail.com>
2021-10-31 11:46:08 +0100

    principal

---
+++ _css/index.js
@@ -0,0 +1,19 @@
+
+/*
+@Autor : Yuri Domingos
+Data : 30 - 10 - 2021
+Objectivo : COnstruir o ficheiro 
+*/
+
+// Select the Elements
+
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+
+// classes name to requeriments
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
