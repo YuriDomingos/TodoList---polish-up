aa650fb1a1c6573d51add76b63678ca6942c7821
YuriDomingos <josemarlabareda@gmail.com>
2021-10-31 12:30:43 +0100

    code

--- _css/index.js
+++ _css/index.js
@@ -8,7 +8,7 @@ Objectivo : COnstruir o ficheiro
 // Select the Elements
 
 const clear = document.querySelector(".clear");
-const dateElement = document.getElementById("date");
+const dateElement = document.getElementById("date")
 const list = document.getElementById("list");
 const input = document.getElementById("input");
 
@@ -24,4 +24,21 @@ const LINE_THROUGH = "lineThrough";
 const options = { weekday : "long", month : "short", day : "numeric"};
 
 const today = new Date();
-dateElement.innerHTML = today.toLocaleDateString("en-us", options);
+dateElement.innerHTML = today.toLocaleDateString("pt-br", options);
+
+function addTodoElements(toDo) {
+
+    const item = ` 
+
+<!-- <i class="fa fa-circle-thin co" job="complete" id="0"> </i>
+<p class="text"> ${toDo} </p>
+<i class="fa fa-trash-o de" jobe="delete" id="0"></i> `
+
+    const position = "beforeend"
+    list.insertAdjacentHTML(position, item);    
+}
+
+addTodoElements("Projecto delta ");
+
+
+
